41340d424aded0b88f1f4eb9a23f5a36e683098f
Ayush Khatri <aayushkhatri571@gmail.com>
2025-12-25 23:46:00 +0545

    added other operstion in backend

--- todo-backend/controller/todocontroller.js
+++ todo-backend/controller/todocontroller.js
@@ -9,3 +9,37 @@ exports.createtodoitem = async (req, res, next)=>{
     res.status(200).json(todolist)
 
 }
+
+exports.gettodoitem = async (req, res, next)=>{
+    try{
+        const item = await todoItem.find();
+    res.json(item)
+    }
+    catch(error){
+        res.status(500).json({message: error.message});
+
+    }
+    
+}
+
+exports.deletetodoitem =async(req, res, next)=>{
+    try{
+        const {id} = req.params
+        await todoItem.findByIdAndDelete(id);
+        res.status(204).json({_id:id})
+
+    }catch(error){
+        res.status(500).json({message: error.message});
+    }
+
+}
+
+exports.marktodoitem = async(req, res, next)=>{
+    try{
+        const{id} = req.params;
+        const todoItem = await todoItem.findById(id);
+        todoItem.complete = true;
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+}
--- todo-backend/router/todolistRouter.js
+++ todo-backend/router/todolistRouter.js
@@ -3,6 +3,9 @@ const todolist = express.Router();
 
 const controller = require("../controller/todocontroller")
 
+todolist.get('/', controller.gettodoitem);
 todolist.post('/', controller.createtodoitem);
+todolist.delete('/:id', controller.deletetodoitem);
+todolist.put('/:id/completed', controller.marktodoitem);
 
 module.exports = todolist
