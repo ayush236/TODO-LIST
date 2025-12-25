7f689a3f425dc720d821b7bd00490f9cfbce5b2f
Ayush Khatri <aayushkhatri571@gmail.com>
2025-12-26 00:50:03 +0545

    added the service

--- todo-frontend/src/services/todoserver.js
+++ todo-frontend/src/services/todoserver.js
@@ -1,3 +1,5 @@
+
+//adding item to the server from frontend
 export const Addtoserver = async (task, date)=>{
     const Response = await fetch('http://localhost:3000/todo/items', {
         method: "POST",
@@ -9,6 +11,35 @@ export const Addtoserver = async (task, date)=>{
     const item = await Response.json()
     return mapServerItemToLocalItem(item)
 }
+
+// geting the item form the server to display the date
+export const GetItemsFromServer = async () =>{
+    const Response = await fetch('http://localhost:3000/todo/items')
+    const item = await Response.json();
+    return item.map(mapServerItemToLocalItem);
+}
+
+//marking the item on the server
+export const MarkItemOnServer = async (id)=>{
+    const Response = await fetch(`http://localhost:3000/todo/items/${id}/completed`,{
+        method: "PUT"
+    })
+    const item = await Response.json();
+    return mapServerItemToLocalItem(item);
+
+}
+
+//deleting the items from the server
+export const deletetodoitem = async (id) => {
+    const Response = await fetch(`http://localhost:300/todo/items/${id}`,{
+        method: "DELETE"
+    })
+    const item = await Response.json();
+    return item. _id;
+}
+
+
+
 
 const mapServerItemToLocalItem =(serveritems) =>{
     return{
